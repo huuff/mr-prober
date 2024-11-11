86e1b9102c300d31fee6df197cb8c04db9cc4d26
Haf <haf@protonmail.ch>
2024-11-11 20:00:27 +0100

    feat: in-memory poller

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{future::Future, marker::PhantomData};
+
+pub trait Poller {
+    type Item;
+    type Sentinel;
+
+    fn next(&self) -> impl Future<Output = Option<Self::Item>>;
+
+    fn commit(&mut self, sentinel: Self::Sentinel) -> impl Future<Output = ()>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait PollerRetriever<Item, Sentinel> {
+    fn next(&self, sentinel: Option<&Sentinel>) -> impl Future<Output = Option<Item>>;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<Item, Sentinel, F, Fut> PollerRetriever<Item, Sentinel> for F
+where
+    F: Fn(Option<&Sentinel>) -> Fut,
+    Fut: Future<Output = Option<Item>>,
+{
+    fn next(&self, sentinel: Option<&Sentinel>) -> impl Future<Output = Option<Item>> {
+        (self)(sentinel)
+    }
+}
+
+pub struct InMemoryPoller<Item, Sentinel, Retriever> {
+    last_sentinel: Option<Sentinel>,
+    retriever: Retriever,
+    _item: PhantomData<Item>,
+}
+
+impl<Item, Sentinel, Retriever> InMemoryPoller<Item, Sentinel, Retriever> {
+    pub fn new(retriever: Retriever) -> Self {
+        Self {
+            last_sentinel: None,
+            retriever,
+            _item: PhantomData,
+        }
+    }
+}
+
+impl<Item, Sentinel, Retriever> Poller for InMemoryPoller<Item, Sentinel, Retriever>
+where
+    Retriever: PollerRetriever<Item, Sentinel>,
+{
+    type Item = Item;
+    type Sentinel = Sentinel;
+
+    async fn next(&self) -> Option<Self::Item> {
+        self.retriever.next(self.last_sentinel.as_ref()).await
+    }
+
+    async fn commit(&mut self, sentinel: Self::Sentinel) {
+        self.last_sentinel.replace(sentinel);
     }
 }
---
+++ tests/sqlx.rs
@@ -0,0 +1,27 @@
+use mr_prober::{InMemoryPoller, Poller};
+use sqlx::Row;
+
+#[sqlx::test]
+async fn in_memory_sqlx_test(db: sqlx::PgPool) {
+    // ARRANGE
+    sqlx::query!("CREATE SEQUENCE test_seq")
+        .execute(&db)
+        .await
+        .unwrap();
+
+    let mut poller = InMemoryPoller::<i64, i64, _>::new(move |_last: Option<&i64>| {
+        let db = db.clone();
+        async move {
+            let res = sqlx::query("SELECT nextval('test_seq') AS next")
+                .fetch_one(&db)
+                .await
+                .unwrap();
+            Some(res.get::<i64, &str>("next"))
+        }
+    });
+
+    for i in 0..10 {
+        assert_eq!(i + 1, poller.next().await.unwrap());
+        poller.commit(i).await;
+    }
+}
