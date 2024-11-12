7d2450fc667d0c1a99bb5109e733204cd990a261
Haf <haf@protonmail.ch>
2024-11-12 08:51:32 +0100

    refactor: cleaner runtime config

--- src/file.rs
+++ src/file.rs
@@ -1,18 +1,11 @@
-use std::{marker::PhantomData, str::FromStr};
+use std::{future::Future, marker::PhantomData, str::FromStr};
 
 use crate::{Prober, ProberRetriever};
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "runtime-tokio")] {
-        type File = tokio::sync::Mutex<tokio::fs::File>;
-    } else {
-        compile_error!("need to select a runtime")
-    }
-}
 
 pub struct FileBackedProber<Item, Sentinel, Retriever> {
     retriever: Retriever,
-    file: File,
+    file: <RuntimeImpl as Runtime>::File,
     _item: PhantomData<Item>,
     _sentinel: PhantomData<Sentinel>,
 }
@@ -27,7 +20,7 @@ where
     type Sentinel = Sentinel;
 
     async fn next(&self) -> Option<Self::Item> {
-        let last_sentinel_string = self.read_string().await;
+        let last_sentinel_string = RuntimeImpl::read_string(&self.file).await;
         // ROYY silly, but I'd love an is_not_empty method
         let last_sentinel = (!last_sentinel_string.is_empty())
             .then(|| Self::Sentinel::from_str(&last_sentinel_string).unwrap());
@@ -35,7 +28,7 @@ where
     }
 
     async fn commit(&mut self, sentinel: Self::Sentinel) {
-        self.write_string(&sentinel.to_string()).await;
+        RuntimeImpl::write_str(&self.file, &sentinel.to_string()).await;
     }
 }
 
@@ -46,73 +39,89 @@ impl<Item, Sentinel, Retriever> FileBackedProber<Item, Sentinel, Retriever> {
     async fn new(file_path: &str, retriever: Retriever) -> Self {
         Self {
             retriever,
-            file: open_file(file_path).await,
+            file: RuntimeImpl::open_file(file_path).await,
             _item: PhantomData,
             _sentinel: PhantomData,
         }
     }
 
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "runtime-tokio")] {
-            async fn read_string(&self) -> String {
+}
+
+
+trait Runtime {
+    type File;
+
+    fn open_file(path: &str) -> impl Future<Output = Self::File>;
+
+
+    fn read_string(file: &Self::File) -> impl Future<Output = String>;
+
+    fn write_str(file: &Self::File, text: &str) -> impl Future<Output = ()>;
+}
+
+
+struct RuntimeImpl;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "runtime-tokio")] {
+        impl Runtime for RuntimeImpl {
+            type File = tokio::sync::Mutex<tokio::fs::File>;
+            
+            async fn open_file(path: &str) -> Self::File {
+                tokio::sync::Mutex::new(
+                    tokio::fs::File::options()
+                        .read(true)
+                        .write(true)
+                        .create(true)
+                        .truncate(false)
+                        .open(path)
+                        .await
+                        .unwrap()
+                )
+            }
+
+
+            async fn read_string(file: &Self::File) -> String {
                 use tokio::io::AsyncReadExt;
 
                 let mut output = String::new();
-                self.file
+                file
                     .lock()
                     .await
                     .read_to_string(&mut output)
                     .await
                     .unwrap();
                 output
-
             }
-        } else {
-            async fn read_string(&self) -> String {
-                compile_error!("need to select a runtime")
-            }
-        }
-    }
 
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "runtime-tokio")] {
-            async fn write_string(&self, sentinel: &str) {
+            async fn write_str(file: &Self::File, text: &str) {
                 use tokio::io::AsyncWriteExt;
 
-                self.file
+                file
                     .lock()
                     .await
-                    .write_all(sentinel.as_bytes())
+                    .write_all(text.as_bytes())
                     .await
                     .unwrap();
-
-            }
-        } else {
-            async fn write_string(&self, sentinel: &str) {
-                compile_error!("need to select a runtime")
             }
         }
-    }
-}
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "runtime-tokio")] {
-        async fn open_file(path: &str) -> File {
-            tokio::sync::Mutex::new(
-                tokio::fs::File::options()
-                    .read(true)
-                    .write(true)
-                    .create(true)
-                    .truncate(false)
-                    .open(path)
-                    .await
-                    .unwrap()
-            )
-        }
-
     } else {
-        async fn open_file(path: &str) -> File {
-            compile_error!("need to select a runtime")
+        impl Runtime for RuntimeImpl {
+            type File = ();
+            type FileWrapper<File> = ();
+            
+            async fn open_file(path: &str) -> Self::FileWrapper<File> {
+                compile_error!("need to select a runtime feature") 
+            }
+
+
+            async fn read_string(file_wrapper: &Self::FileWrapper<File>) -> String {
+                compile_error!("need to select a runtime feature") 
+                
+            }
+
+            async fn write_str(file_wrapper: &Self::FileWrapper<File>, text: &str) {
+                compile_error!("need to select a runtime feature") 
+            }
         }
     }
 }
