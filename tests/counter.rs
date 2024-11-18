f70ad0bb6382f7bf760c504e2e83b2c5b1488492
Haf <haf@protonmail.ch>
2024-11-18 20:21:58 +0100

    test: a processor implementation

--- tests/counter.rs
+++ tests/counter.rs
@@ -3,35 +3,15 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use mr_prober::{Prober as _, ProberImpl};
+use mr_prober::{proc::Processor, Prober as _, ProberImpl};
 use rand::distributions::DistString;
-
-#[derive(Default)]
-struct Counter {
-    pub interactions: Vec<u64>,
-}
-
-impl Counter {
-    /// Pushes `val` into the interactions and returns the number of interactions so far
-    pub fn interact(&mut self, val: u64) -> u64 {
-        self.interactions.push(val);
-        self.interactions.len().try_into().unwrap()
-    }
-}
 
 #[tokio::test]
 async fn in_memory() {
     // ARRANGE
     let counter = Arc::new(Mutex::new(Counter::default()));
 
-    let mut prober = ProberImpl::in_memory(|sentinel: Option<u64>| {
-        let counter = Arc::clone(&counter);
-        async move {
-            let next = counter.lock().unwrap().interact(sentinel.unwrap_or(0));
-
-            Ok::<_, Infallible>(Some(next))
-        }
-    });
+    let mut prober = ProberImpl::in_memory(CounterProcessor::new(Arc::clone(&counter)));
 
     // ACT
     for _ in 0..10 {
@@ -52,16 +32,9 @@ async fn in_file() {
 
     let test_id = rand::distributions::Alphanumeric.sample_string(&mut rand::thread_rng(), 10);
     let file_path = format!("/tmp/mrprober-test-{test_id}");
-    let mut prober = ProberImpl::from_file(&file_path, |sentinel: Option<u64>| {
-        let counter = Arc::clone(&counter);
-        async move {
-            let next = counter.lock().unwrap().interact(sentinel.unwrap_or(0));
-
-            Ok::<_, Infallible>(Some(next))
-        }
-    })
-    .await
-    .unwrap();
+    let mut prober = ProberImpl::from_file(&file_path, CounterProcessor::new(Arc::clone(&counter)))
+        .await
+        .unwrap();
 
     // ACT
     for _ in 0..10 {
@@ -73,4 +46,36 @@ async fn in_file() {
         counter.lock().unwrap().interactions,
         vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     );
+}
+
+#[derive(Default)]
+struct Counter {
+    interactions: Vec<u64>,
+}
+
+impl Counter {
+    /// Pushes `val` into the interactions and returns the number of interactions so far
+    fn interact(&mut self, val: u64) -> u64 {
+        self.interactions.push(val);
+        self.interactions.len().try_into().unwrap()
+    }
+}
+
+pub struct CounterProcessor {
+    counter: Arc<Mutex<Counter>>,
+}
+
+impl CounterProcessor {
+    fn new(counter: Arc<Mutex<Counter>>) -> Self {
+        Self { counter }
+    }
+}
+
+#[async_trait::async_trait]
+impl Processor<u64, Infallible> for CounterProcessor {
+    async fn next(&self, current: Option<u64>) -> Result<Option<u64>, Infallible> {
+        let next = self.counter.lock().unwrap().interact(current.unwrap_or(0));
+
+        Ok::<_, Infallible>(Some(next))
+    }
 }
