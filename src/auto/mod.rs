be4af7276c04dae20cea3dff2261667b448ec517
Haf <haf@protonmail.ch>
2024-11-24 18:29:39 +0100

    fix: actually using backoff

--- src/auto/mod.rs
+++ src/auto/mod.rs
@@ -41,25 +41,62 @@ where
                     },
                     ProbeResult::Empty => match self.cfg.on_empty {
                         AutoProberStrategy::Abort => {
-                            tracing::info!("probe is empty, aborting");
+                            tracing::info!(event = "probe-empty", "abort");
                             return;
                         }
                         AutoProberStrategy::DelaySecs(secs) => {
-                            tracing::info!(
-                                "probe is empty, waiting {secs} seconds before trying again"
-                            );
+                            tracing::info!(event = "probe-empty", "retrying in {secs} seconds");
                             RuntimeImpl::sleep(secs.into()).await;
                         }
                         AutoProberStrategy::Continue => {}
                         AutoProberStrategy::Backoff(ref mut backoff) => {
-                            // TODO: kinda duplicated
                             match backoff.next_sleep() {
-                                Some(delay) => RuntimeImpl::sleep(delay.as_secs()).await,
-                                None => return, // maybe an err would be better
+                                Some(delay) => {
+                                    let delay_secs = delay.as_secs();
+                                    tracing::error!(
+                                        event = "probe-empty",
+                                        "trying again in {delay_secs} seconds"
+                                    );
+                                    RuntimeImpl::sleep(delay_secs).await
+                                }
+                                None => {
+                                    tracing::error!(
+                                        event = "probe-empty",
+                                        "retries exhausted, aborting"
+                                    );
+                                    // MAYBE an err would be better
+                                    return;
+                                }
                             }
                         }
                     },
-                    ProbeResult::Error(err) => err.panic(),
+                    ProbeResult::Error(err) => match self.cfg.on_error {
+                        AutoProberStrategy::Abort => {
+                            tracing::error!(event = "probe-error", err = ?err, "abort");
+                            err.panic();
+                        }
+                        AutoProberStrategy::DelaySecs(secs) => {
+                            tracing::error!(event = "probe-error", err = ?err, "retrying in {secs} seconds");
+                            RuntimeImpl::sleep(secs.into()).await;
+                        }
+                        AutoProberStrategy::Continue => {
+                            tracing::error!(event = "probe-error", err = ?err, "trying again");
+                        }
+                        AutoProberStrategy::Backoff(ref mut backoff) => {
+                            match backoff.next_sleep() {
+                                Some(delay) => {
+                                    let delay_secs = delay.as_secs();
+                                    tracing::error!(event = "probe-error", err = ?err, "trying again in {delay_secs} seconds");
+                                    RuntimeImpl::sleep(delay_secs).await
+                                }
+                                None => {
+                                    tracing::error!(event = "probe-error", err = ?err, "retries exhausted, aborting");
+                                    // MAYBE an err would be better
+                                    return;
+                                }
+                            }
+                        }
+                    },
                 }
             }
         })
